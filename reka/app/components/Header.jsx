e9d80ed674935239238abebb5a2c64d0a1c42c45
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-09-10 10:26:39 +0200

    Installed Next.js and created a Header and ProductCard component

---
+++ reka/app/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import cart from "../public/cart.svg";
+import profile from "../public/profile.svg";
+import menu from "../public/menu.svg";
+
+const Header = () => {
+  return (
+    <>
+      <header className="w-full fixed top-0 bg-white  z-10">
+        <nav className="flex justify-between items-center px-[8%] py-3 md:py-6">
+          <Link href="">
+            <Image src={menu} />
+          </Link>
+          <Link href="/" className=" text-[#87e64b] font-extrabold text-4xl md:text-6xl">
+            Reka.
+          </Link>
+
+          <div className="navbar-left flex items-center gap-4">
+            {/* cart */}
+            <Link  href="" className="hidden md:block">
+              <Image src={cart} />
+            </Link>
+            {/* Login */}
+            <Link href="">
+              <Image src={profile} />
+            </Link>
+          </div>
+        </nav>
+        <hr className="border mb-8 flex min-w-[100vw] mx-auto" />
+      </header>
+    </>
+  );
+};
+
+export default Header;
