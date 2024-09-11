31b37e23d563243465aac7be06f84638049c2da9
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-09-11 14:42:27 +0200

    i fixed the bug for duplicating my Products , by implementing an if statement to start the Product page at 0

--- reka/app/api/route.js
+++ reka/app/api/route.js
@@ -1,10 +1,13 @@
 
 const API_BASE_URL = "https://next-ecommerce-api.vercel.app/products"
 
-export default async function fetchData (skip = 1) {
+export default async function fetchData (skip = 0) {
     
     const response = await fetch(`${API_BASE_URL}?skip=${skip}&limit=20`)
-    console.log(response)
+    // let r = await response.json()
+
+    // console.log(r)
+    console.log('12345')
 
     if(!response) {
         throw Error ("Failed to fetch Data")
--- reka/app/components/ProductList.jsx
+++ reka/app/components/ProductList.jsx
@@ -7,20 +7,27 @@ const ProductList = () => {
   let [products, setProducts] = useState([]);
   let [loading, setLoading] = useState(false);
   /**To keep the state of the pages */
-  let [pages, setPages] = useState(1);
+  let [pages, setPages] = useState(0);
 
 
   const getProducts = async (pageNumber) => {
     try {
       const data = await fetchData(pageNumber);
+      console.log('123')
+      if(pageNumber!=0){
       setProducts((previousProducts) => [...previousProducts, ...data]);
+      }
+      else{
+        setProducts(data)
+      }
+      console.log('setp')
     } catch (error) {
       console.error("Error Fetching Products:", error);
     }
   };
 
   useEffect(() => {
-    getProducts(1);
+    getProducts(0);
   }, []);
 
  
@@ -38,12 +45,13 @@ const ProductList = () => {
 
   return (
     <div>
+        {console.log(products)}
       <div className="lg:max-h-[130rem] relative bottom-14 px-[8%] md:px-0 max-w-xl md:mx-auto grid gap-4 grid-cols-2 lg:grid-cols-5 justify-center md:grid-cols-3 lg:mx-[9%] items-center  lg:max-w-none my-4">
         {products.map((product) => (
           <ProductCard key={product.id} {...product}  />
         ))}
       </div>
-      <div className="w-full mb-10 text-center ">
+      <div className="w-full mb-10 text-center relative bottom-0">
         <button onClick={handleShowMore} className="border px-4 py-2 shadow-md">
           Show More
         </button>
