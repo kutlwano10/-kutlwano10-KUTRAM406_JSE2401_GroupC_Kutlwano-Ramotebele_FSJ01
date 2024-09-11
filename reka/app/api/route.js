8c5e7f821732a60b40890fb9499e6ee586ca17a1
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-09-11 13:00:41 +0200

    Restructured my GetProducts() to get the previousProducts and the copy of the skip    to data

--- reka/app/api/route.js
+++ reka/app/api/route.js
@@ -1,9 +1,9 @@
 
 const API_BASE_URL = "https://next-ecommerce-api.vercel.app/products"
 
-export default async function fetchData (skip) {
+export default async function fetchData (skip = 1) {
     
-    const response = await fetch(`${API_BASE_URL}?skip=${skip}`)
+    const response = await fetch(`${API_BASE_URL}?skip=${skip}&limit=20`)
     console.log(response)
 
     if(!response) {
--- reka/app/components/ProductList.jsx
+++ reka/app/components/ProductList.jsx
@@ -5,39 +5,42 @@ import fetchData from "../api/route";
 
 const ProductList = () => {
   let [products, setProducts] = useState([]);
-  let [moreProducts, setMoreProducts] = useState([]);
-  let page = 1;
+  let [loading, setLoading] = useState(false);
+  /**To keep the state of the pages */
+  let [pages, setPages] = useState(1);
 
+
+  const getProducts = async (pageNumber) => {
+    try {
+      const data = await fetchData(pageNumber);
+      setProducts((previousProducts) => [...previousProducts, ...data]);
+    } catch (error) {
+      console.error("Error Fetching Products:", error);
+    }
+  };
+
+  useEffect(() => {
+    getProducts(1);
+  }, []);
+
+ 
   /**
    *
    * @returns number of products
    */
   const handleShowMore = () => {
     console.log("im clicked");
-    return { ...products, moreProducts };
+    const nextPage = pages + 20;
+    setPages(nextPage);
+    getProducts(nextPage)
+    
   };
-
-  useEffect(() => {
-    const getProducts = async () => {
-      const data = await fetchData();
-      return setProducts(data);
-    };
-    return getProducts;
-  }, []);
-
-  useEffect(() => {
-    const getMoreProducts = async () => {
-      const data = await fetchData(20);
-      return setMoreProducts(data);
-    };
-    return getMoreProducts;
-  });
 
   return (
     <div>
       <div className="lg:max-h-[130rem] relative bottom-14 px-[8%] md:px-0 max-w-xl md:mx-auto grid gap-4 grid-cols-2 lg:grid-cols-5 justify-center md:grid-cols-3 lg:mx-[9%] items-center  lg:max-w-none my-4">
         {products.map((product) => (
-          <ProductCard key={product.id} {...product} />
+          <ProductCard key={product.id} {...product}  />
         ))}
       </div>
       <div className="w-full mb-10 text-center ">
